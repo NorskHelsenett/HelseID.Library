f1000a516d0bd07058ce917972549dcbc8dbdf2f
Rune Andreas Grimstad <rag@rag.no>
2025-05-21 12:15:27 +0200

    Removed an unused class

--- HelseID.Standard.Tests/Mocks/DiscoveryDocumentGetterMock.cs
+++ HelseID.Standard.Tests/Mocks/DiscoveryDocumentGetterMock.cs
@@ -3,13 +3,14 @@ using HelseID.Standard.Models;
 
 namespace HelseID.Standard.Tests.Mocks;
 
-public class DiscoveryDocumentGetterWithMockHttpClient : IDiscoveryDocumentGetter
+public class DiscoveryDocumentGetterMock : IDiscoveryDocumentGetter
 {
     public Task<DiscoveryDocument> GetDiscoveryDocument()
     {
         return Task.FromResult(new DiscoveryDocument
         {
             AuthorizeEndpoint = "https://helseid-sts.nhn.no/connect/authorize",
+            TokenEndpoint = "https://helseid-sts.nhn.no/connect/token",
         });
     }
 }
