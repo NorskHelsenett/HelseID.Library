12e4fe5c7e6ec114646d13e6485148788dbd9b56
Håvard Brækken <havard.braekken@nhn.no>
2025-09-11 11:03:31 +0200

    Added HelseIdServiceCollectionExtensions

---
+++ dotnet/HelseId.Library.SelfService/HelseIdServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HelseId.Library.SelfService;
+
+public static class HelseIdServiceCollectionExtensions
+{
+    public static IHelseIdBuilder AddSelvbetjeningKeyRotation (this IHelseIdBuilder helseIdBuilder)
+    {
+        helseIdBuilder.RemoveServiceRegistrations<ISelvbetjeningSecretUpdater>();
+        helseIdBuilder.Services.AddSingleton<ISelvbetjeningSecretUpdater, SelvbetjeningSecretUpdater>();
+        return helseIdBuilder;
+    }
+}
