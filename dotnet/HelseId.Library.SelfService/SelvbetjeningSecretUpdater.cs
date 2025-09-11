1da3147d9465773322fdd1c901e2da9c77c6bde7
Håvard Brækken <havard.braekken@nhn.no>
2025-09-11 11:10:10 +0200

    Manually building public-private JWK

--- dotnet/HelseId.Library.SelfService/SelvbetjeningSecretUpdater.cs
+++ dotnet/HelseId.Library.SelfService/SelvbetjeningSecretUpdater.cs
@@ -1,5 +1,7 @@
 ﻿using System.Net.Http.Json;
 using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
 using HelseId.Library.ClientCredentials.Interfaces;
 using HelseId.Library.Exceptions;
 using HelseId.Library.Interfaces.Configuration;
@@ -26,18 +28,40 @@ public class SelvbetjeningSecretUpdater : ISelvbetjeningSecretUpdater
         _dPoPProofCreator = dPoPProofCreator;
     }
 
-    public async Task UpdateClientSecret()
+    public async Task<DateTime> UpdateClientSecret()
     {
-        // 1. Create new json web key pair
-        // 2. Get access token for SB-api
-        // 3. Call SB-api to update new json web key public key
-        // 4. Store key locally
-
         var newKeyPair = RSA.Create(4096);
+        var kid = RandomNumberGenerator.GetHexString(32);
         var rsaWithPrivateKey = new RsaSecurityKey(newKeyPair.ExportParameters(true));
+        rsaWithPrivateKey.KeyId = kid;
         var rsaWithoutPrivateKey = new RsaSecurityKey(newKeyPair.ExportParameters(false));
+        rsaWithoutPrivateKey.KeyId = kid;
         var jwkWithPrivateKey = JsonWebKeyConverter.ConvertFromRSASecurityKey(rsaWithPrivateKey);
         var jwkWithoutPrivateKey = JsonWebKeyConverter.ConvertFromRSASecurityKey(rsaWithoutPrivateKey);
+        
+        Dictionary<string, string> jwkPublicValues = new Dictionary<string, string>();
+        jwkPublicValues.Add("kty", "RSA");
+        jwkPublicValues.Add("e", jwkWithoutPrivateKey.E);
+        jwkPublicValues.Add("use", "sig");
+        jwkPublicValues.Add("kid", jwkWithoutPrivateKey.Kid);
+        jwkPublicValues.Add("alg", "PS256");
+        jwkPublicValues.Add("n",  jwkWithoutPrivateKey.N);
+        string jsonPublicKey = JsonSerializer.Serialize(jwkPublicValues);
+        
+        Dictionary<string, string> jwkPrivateValues = new Dictionary<string, string>();
+        jwkPrivateValues.Add("kty", "RSA");
+        jwkPrivateValues.Add("e", jwkWithPrivateKey.E);
+        jwkPrivateValues.Add("use", "sig");
+        jwkPrivateValues.Add("kid", jwkWithPrivateKey.Kid);
+        jwkPrivateValues.Add("alg", "PS256");
+        jwkPrivateValues.Add("n",  jwkWithPrivateKey.N);
+        jwkPrivateValues.Add("p",  jwkWithPrivateKey.P);
+        jwkPrivateValues.Add("q",  jwkWithPrivateKey.Q);
+        jwkPrivateValues.Add("d",  jwkWithPrivateKey.D);
+        jwkPrivateValues.Add("qi",  jwkWithPrivateKey.QI);
+        jwkPrivateValues.Add("dq",  jwkWithPrivateKey.DQ);
+        jwkPrivateValues.Add("dp",  jwkWithPrivateKey.DP);
+        string jsonPrivateKey = JsonSerializer.Serialize(jwkPrivateValues);
         
         var tokenResponse = await _clientCredentialsFlow.GetTokenResponseAsync("nhn:selvbetjening/client");
         if(tokenResponse is TokenErrorResponse tokenErrorResponse)
@@ -47,22 +71,18 @@ public class SelvbetjeningSecretUpdater : ISelvbetjeningSecretUpdater
 
         var accessTokenResponse = (AccessTokenResponse)tokenResponse;
         var dPopProof = await _dPoPProofCreator.CreateDPoPProofForApiCall("https://api.selvbetjening.test.nhn.no/v1/client-secret", "POST", accessTokenResponse.AccessToken);
-        
-        // blabla kalle api
-        
-        
         var httpClient = new HttpClient();
         var httpRequest = new HttpRequestMessage(HttpMethod.Post, "https://api.selvbetjening.test.nhn.no/v1/client-secret");
-        httpRequest.Content = JsonContent.Create(jwkWithoutPrivateKey);
+        httpRequest.Content = new StringContent(jsonPublicKey, Encoding.UTF8, mediaType: "application/json");
         httpRequest.Headers.Add("Authorization", $"DPoP {accessTokenResponse.AccessToken}");
         httpRequest.Headers.Add("DPoP", dPopProof);
         
         var response = await httpClient.SendAsync(httpRequest);
+        var content = await response.Content.ReadAsStringAsync();
         response.EnsureSuccessStatusCode();
         var result = await response.Content.ReadFromJsonAsync<ClientSecretUpdateResponse>();
-        await _signingCredentialReference.UpdateSigningCredential(jwkWithPrivateKey.ToString());
+        await _signingCredentialReference.UpdateSigningCredential(jsonPrivateKey);
         
-        
-        
+        return result.Expiration;
     }
 }
