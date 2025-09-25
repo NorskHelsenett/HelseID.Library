022a93135287901c2b913f442034ca442c630d31
Håvard Brækken <havard.braekken@nhn.no>
2025-09-25 14:48:13 +0200

    Split ClientSecretEndpointMock.cs

---
+++ dotnet/HelseId.Library.Mocks/ClientSecretEndpointMock.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using HelseId.Library.SelfService.Interfaces;
+
+namespace HelseId.Library.Mocks;
+
+public class ClientSecretEndpointMock : IClientSecretEndpoint
+{
+    private readonly string _uri;
+    public string PublicKey { get; set; } = string.Empty;
+
+    public ClientSecretEndpointMock(string uri)
+    {
+        _uri = uri;
+    }
+    
+    public Task<HttpRequestMessage> GetClientSecretRequest(string publicKey)
+    {
+        PublicKey = publicKey;
+        var httpRequest = new HttpRequestMessage(HttpMethod.Post, _uri);
+        httpRequest.Content = new StringContent(publicKey, Encoding.UTF8, mediaType: "application/json");
+        httpRequest.Headers.Add("Authorization", $"DPoP eyFoobar");
+        httpRequest.Headers.Add("DPoP", "eyFoobarFooobar");
+        return Task.FromResult(httpRequest);    
+    }
+}
