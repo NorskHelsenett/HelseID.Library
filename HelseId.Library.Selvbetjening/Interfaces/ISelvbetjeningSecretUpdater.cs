d6c0d4fd86fc1f2e48060db268e89f6956db7c2d
Rune Andreas Grimstad <rag@rag.no>
2025-10-16 11:46:26 +0200

    Updated a final xml doc

--- HelseId.Library.Selvbetjening/Interfaces/ISelvbetjeningSecretUpdater.cs
+++ HelseId.Library.Selvbetjening/Interfaces/ISelvbetjeningSecretUpdater.cs
@@ -3,7 +3,9 @@
 public interface ISelvbetjeningSecretUpdater
 {
     /// <summary>
-    /// Registers a new signing key using the Selvbetjening API. Storage of the new key must be handled by 
+    /// Registers a new signing key using the Selvbetjening API.
+    /// Storage of the new key must be handled by the client by implementing
+    /// the ISigningCredentialReference.UpdateSigningCredential method.
     /// </summary>
     /// <returns>Returns the expiration date of the new signing key.</returns>
     Task<DateTime> UpdateClientSecret();
